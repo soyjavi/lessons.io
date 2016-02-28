604e6c5329caa06f8ee3e7b88db5d7aaca9cc2e7
@soyjavi <javi.jimenez.villar@gmail.com>
2016-02-28 20:43:24 +0700

    Fixed typo

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LayoutSubscribe from './layouts/subscribe';
+import common from './commons'
 
 ReactDOM.render(<LayoutSubscribe />, document.getElementById('container'));
