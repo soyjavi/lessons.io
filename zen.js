2ec5a4de6274247c836de29e78a83464c77f43e7
@soyjavi <javi.jimenez.villar@gmail.com>
2016-03-05 18:42:32 +0700

    Require node-sass

--- zen.js
+++ zen.js
@@ -3,3 +3,17 @@
 require('coffee-script/register');
 require('babel-register')({ presets: ['es2015', 'stage-0', 'react'] });
 require('zenserver').start();
+
+var hook = require('css-modules-require-hook')
+var sass = require('node-sass')
+var path = require('path')
+
+hook({
+  // rootDir: path.join(__dirname, './'),
+  rootDir: path.join(__dirname, '../app'),
+  extensions: [ '.scss' ],
+  generateScopedName: '[name]__[local]___[hash:base64:5]',
+  preprocessCss: function (data) {
+    return sass.renderSync({ data: data }).css
+  }
+})
